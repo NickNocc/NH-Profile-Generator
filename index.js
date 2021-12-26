5533ab69335158c09f2ea76718b33f778000e827
NickNocc <haretakis4@gmail.com>
2021-12-26 12:09:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
- 
+ const fs = require('fs');
+ const inquirer = require (`inquirer`);
+
+
+ const employeePrompt = () => {
+    return inquirer.prompt([
+        {
+            type: `checkbox`,
+            name: `roleSelect`,
+            message: `Please select your position`,
+            choices: [`Manager`, `Engineer`, `Intern`]
+        },
+        {
+            type: `input`,
+            name: `empName`,
+            message: `Please input your legal name`
+        },
+        {
+            type: `input`,
+            name: `empId`,
+            message: `Please enter your employee ID`
+        },
+        {
+            type: `input`,
+            name: `empMail`,
+            message: `Please enter your email`
+        },
+        {
+            type: `input`,
+            name: `offNum`,
+            message: `Please enter your office number`,
+            when: ({ roleSelect }) => roleSelect == `Manager`  
+        },
+        {
+            type: `input`,
+            name: `gitHub`,
+            message: `Please enter your GitHub account`,
+            when: ({ roleSelect }) => roleSelect == `Engineer`
+        },
+        {
+            type: `input`,
+            name: `school`,
+            message: `Please enter your current school`,
+            when: ({ roleSelect }) => roleSelect == `Intern`
+        },
+        {
+            type: `confirm`,
+            name: `addEmployee`,
+            message: `Would you like to add another employee?`,
+            default: false
+        },
+    ]);
+ };
