74c05ff85ce7e0b670159fda0a54a3538dcf6009
NickNocc <haretakis4@gmail.com>
2021-12-26 13:11:44 -0800

    inquirer now set up

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
- const fs = require('fs');
- const inquirer = require (`inquirer`);
+const fs = require('fs');
+const inquirer = require (`inquirer`);
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+inquirer.registerPrompt('loop', require('inquirer-loop')(inquirer));
 
 
  const employeePrompt = () => {
     return inquirer.prompt([
         {
-            type: `checkbox`,
+            type: `list`,
             name: `roleSelect`,
             message: `Please select your position`,
             choices: [`Manager`, `Engineer`, `Intern`]
@@ -44,10 +48,86 @@
             when: ({ roleSelect }) => roleSelect == `Intern`
         },
         {
-            type: `confirm`,
+            type: `loop`,
             name: `addEmployee`,
             message: `Would you like to add another employee?`,
-            default: false
+            default: false,
+            questions: [
+                {
+                    type: `list`,
+                    name: `roleSelect`,
+                    message: `Please select your position`,
+                    choices: [`Manager`, `Engineer`, `Intern`]
+                },
+                {
+                    type: `input`,
+                    name: `empName`,
+                    message: `Please input your legal name`
+                },
+                {
+                    type: `input`,
+                    name: `empId`,
+                    message: `Please enter your employee ID`
+                },
+                {
+                    type: `input`,
+                    name: `empMail`,
+                    message: `Please enter your email`
+                },
+                {
+                    type: `input`,
+                    name: `offNum`,
+                    message: `Please enter your office number`,
+                    when: ({ roleSelect }) => roleSelect == `Manager`  
+                },
+                {
+                    type: `input`,
+                    name: `gitHub`,
+                    message: `Please enter your GitHub account`,
+                    when: ({ roleSelect }) => roleSelect == `Engineer`
+                },
+                {
+                    type: `input`,
+                    name: `school`,
+                    message: `Please enter your current school`,
+                    when: ({ roleSelect }) => roleSelect == `Intern`
+                },
+            ]
         },
     ]);
  };
+
+ employeePrompt()
+    .then(resData => {
+        console.log(resData);
+        let employees = resData.addEmployee;
+        let firstEntry = [{
+            roleSelect: resData.roleSelect,
+            empName: resData.empName,
+            empId: resData.empId,
+            empMail: resData.empMail,
+            offNum: resData.offNum 
+        }];
+        employees.push(firstEntry);
+
+        let managers = [];
+        let engineers = [];
+        let interns = [];
+
+        for (let i = 0; i < employees.length; i++) {
+            let emp = employees[i]
+            if (emp.roleSelect == "Manager") {
+                const manager = new Manager(emp.empName, emp.empId, emp.empMail, emp.offNum);
+                managers.push(manager);
+            } else if (emp.roleSelect == "Intern") {
+                const intern = new Intern(emp.empName, emp.empId, emp.empMail, emp.gitHub);
+                interns.push(intern);
+            } else if (emp.roleSelect == "Engineer") {
+                const engineer = new Engineer(emp.empName, emp.empId, emp.empMail, emp.school);
+                engineers.push(engineer);
+            }
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
